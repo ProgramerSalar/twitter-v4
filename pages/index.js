888b6b44080aba69bc776b5f36e1dd2e189e0ff5
manish <manishkumar60708090@gmail.com>
2022-09-14 15:36:58 +0530

    fundamental this project

---
+++ pages/index.js
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+
+
+export default function Home(){
+  return(
+    <div className="">
+      <Head>
+        <title>Twitter-clone</title>
+      </Head>
+
+      <div className="">
+
+      </div>
+    </div>
+  )
+}
